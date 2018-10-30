71313795953e0df8ebaac4d8c8e933ca8a0e5ffd
Maria Sanchez <msanchez157@gmail.com>
2018-10-30 18:36:50 -0400

    All product vertical slice

--- client/components/shop.js
+++ client/components/shop.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {fetchProducts} from '../store/products'
+
 
 class Shop extends Component {
+
+  componentDidMount(){
+    this.props.fetchProducts()
+  }
   render() {
+    console.log('!!!!!', this.props.products)
+  
     return (
-      //list all products here
-      <div />
+      <div>
+        
+        { this.props.products.map(product => (
+          <div key={product.id}>
+          <figure className="product">
+            <div className="product-figure">
+              <Link to={`/shop/${product.id}`}>{product.title}</Link>
+            </div>
+            <img src={product.imageURL} alt={product.title} width="200px" height="200px" />
+          </figure>
+          </div>
+        ))}
+      </div>
+      
     )
   }
 }
 
-export default Shop
+const mapStateToProps = state => {
+  return {
+    products: state.products
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchProducts: () => dispatch(fetchProducts())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Shop)
