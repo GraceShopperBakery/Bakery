23b179b05cd051e05b8d7e4340ccc7b3f0ea6b0f
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2018-10-31 12:48:27 -0400

    Refactor(Backend): created Categories table, removed categories field on Product table, established many-to-many association for Product-Category

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,24 +3,18 @@ const Order = require('./order')
 const Product = require('./products')
 const Review = require('./review')
 const OrderQty = require('./orderQty')
+const Category = require('./categories')
 
 /**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
-
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
+ * Associations
  */
 
 Product.hasMany(Review);
 User.hasMany(Order);
 User.hasMany(Review);
+
+Product.belongsToMany(Category, {through: 'product-categories'})
+Category.belongsToMany(Product, {through: 'product-categories'})
 
 Order.belongsTo(User);
 Order.hasMany(OrderQty)
@@ -31,6 +25,13 @@ Review.belongsTo(Product);
 OrderQty.belongsTo(Order);
 OrderQty.belongsTo(Product);
 Product.hasMany(OrderQty);
+
+/**
+ * We'll export all of our models here, so that any time a module needs a model,
+ * we can just require it from 'db/models'
+ * for example, we can say: const {User} = require('../db/models')
+ * instead of: const User = require('../db/models/user')
+ */
 
 module.exports = {
   User, Product, Review, Order, OrderQty
