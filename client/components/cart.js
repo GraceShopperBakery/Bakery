5d403054332e06a41eef4b5c22dbfd695a018014
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2018-11-01 20:03:16 -0400

    Feature: Cart - cart updated to increase, decrease qty and remove product in cart

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,23 +1,66 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {removeProduct, increaseQty, decreaseQty} from '../store/cart'
 
 class Cart extends Component {
+  constructor(props) {
+    super(props)
+    this.handleRemove = this.handleRemove.bind(this)
+    this.handleIncreaseQty = this.handleIncreaseQty.bind(this)
+    this.handleDecreaseQty = this.handleDecreaseQty.bind(this)
+  }
+
+  handleRemove(productName) {
+    this.props.removeProduct(productName)
+  }
+
+  handleIncreaseQty(productName) {
+    this.props.increaseQty(productName)
+  }
+
+  handleDecreaseQty(productName) {
+    this.props.decreaseQty(productName)
+  }
+
   render() {
     const cartProducts = Object.keys(this.props.cart)
     let total = 0
+
     return (
       <div>
         {cartProducts.map(productName => {
           const product = this.props.cart[productName]
           total += product.price * product.quantity
+
           return (
             <div key={product.id}>
-              <li>Product: {product.title}</li>
+              <Link to={`/products/{product.id}`}>
+                <li>Product: {product.title}</li>
+              </Link>
               <li>Price per unit: {product.price}</li>
-              <li>Quantity: {product.quantity}</li>
-              <li>Subtotal: {product.price * product.quantity}</li>
-              <br />
+              <div>
+                <li>Quantity: {product.quantity}</li>
+                <button
+                  type="button"
+                  onClick={() => this.handleIncreaseQty(product.title)}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  onClick={() => this.handleDecreaseQty(product.title)}
+                >
+                  -
+                </button>
+              </div>
+              <li>Subtotal: {(product.price * product.quantity).toFixed(2)}</li>
+              <button
+                type="button"
+                onClick={() => this.handleRemove(product.title)}
+              >
+                Remove
+              </button>
             </div>
           )
         })}
@@ -34,7 +77,11 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => {
-  return {}
+  return {
+    removeProduct: productName => dispatch(removeProduct(productName)),
+    increaseQty: productName => dispatch(increaseQty(productName)),
+    decreaseQty: productName => dispatch(decreaseQty(productName))
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart)
