c888abe558e31abb8dfedd86c49987acc8857662
Maria Sanchez <msanchez157@gmail.com>
2018-11-02 15:25:32 -0400

    refactor- fixed bug in update admin

--- client/components/updateProduct.js
+++ client/components/updateProduct.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { updateProduct, fetchProducts } from '../store/products'
-import { runInThisContext } from 'vm';
+
 
 
 class UpdateProduct extends Component { 
