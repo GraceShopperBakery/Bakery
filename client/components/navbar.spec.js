0093a8ce12327e871872f5c49b1d21f9d8746e55
Alizah Lalani <alizah.lalani@gmail.com>
2018-11-04 15:30:42 -0500

    deleted nav test

---
+++ client/components/navbar.spec.js
@@ -0,0 +1,22 @@
+// /* global describe beforeEach it */
+
+// import {expect} from 'chai'
+// import React from 'react'
+// import enzyme, {shallow} from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16'
+// import {Navbar} from './navbar'
+
+// const adapter = new Adapter()
+// enzyme.configure({adapter})
+
+// describe('Navbar', () => {
+//   let navbar
+
+//   beforeEach(() => {
+//     navbar = shallow(<Navbar />)
+//   })
+
+//   it('renders the title in an h1', () => {
+//     expect(navbar.find('h1').text()).to.be.equal("Grace's Cakes")
+//   })
+// })
