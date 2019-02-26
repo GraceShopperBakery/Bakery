61e445105dfeb94f75ce920d68fe44100e63bb8b
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2019-02-26 17:21:58 -0500

    Refactor(Frontend):single product updated

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -1,33 +1,91 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchProduct } from '../store/products'
-
+import { addOrUpdateProduct } from '../store/cart'
 class SingleProduct extends Component {
+
+  // constructor(props) { 
+  //   super(props)
+  //   // this.handleAddToCart = this.handleAddToCart.bind(this)
+  // }
 
   componentDidMount() {
     const id = this.props.match.params.productId
     this.props.fetchProduct(id)
   }
 
+
+  handleAddToCart(event) {
+    event.preventDefault()
+    if (event.target.orderQty.value) {
+      let productOrderQty = Number(event.target.orderQty.value)
+      let productInCart = this.props.cart.products.find(
+        product => product.id === this.props.product.id
+      )
+      if (productInCart) {
+        productOrderQty += productInCart.orderQty.quantity
+      }
+      this.props.addProduct(this.props.product.id, productOrderQty, this.props.product.price)
+      event.target.orderQty.value = null
+    }
+  }
+  
   render() {
+    const product = this.props.product;
+    const availableOrderQty = [...Array(11).keys()].slice(1)
+
     return (
-      this.props.product === {} ?
+      product === {} ?
         <div id="invalid">
           <h1>This product does not exist.</h1>
         </div>
         :
         <div>
           <div className="shop">
-            <figure>
-              <div className="product-figure">
-                <h3>{this.props.product.title}</h3>
+            <h2>{product.title}</h2>
+            <div className='product-info'>
+              <img src={product.imageURL} alt={product.title} width="200px" height="200px" />
+              <div className="product-description">
+                <p>{product.description}</p>
+                <div className="product-price-checkout">
+                  <li>Price: ${product.price}</li>
+                  <div>
+                    <form
+                      className="checkout"
+                      onSubmit={event =>
+                        this.handleAddToCart(event)
+                      }
+                    >
+                      <div className="quantity">
+                        <button
+                          className="addToCart"
+                          type="submit"
+                          name={product.title}
+                        >
+                          Add To Cart
+                        </button>
+                        <label>
+                          Qty:&nbsp;
+                          <select
+                            className="qtyFilter"
+                            label="Quantity"
+                            name="orderQty"
+                          >
+                            <option value={null} />
+                            {availableOrderQty.map(num => (
+                              <option key={num} value={num}>
+                                {num}
+                              </option>
+                            ))}
+                          </select>
+                        </label>
+                      </div>
+                    </form>
+                  </div>
+                </div>
               </div>
-              <img src={this.props.product.imageURL} alt={this.props.product.title} width="200px" height="200px" />
-            </figure>
-            <div>
-              <p>{this.props.product.description}</p>
-              <li>Price: ${this.props.product.price}</li>
             </div>
+          <h2 id="end">&nbsp;</h2>
           </div>
         </div>
     )
@@ -36,13 +94,16 @@ class SingleProduct extends Component {
 
 const mapStateToProps = state => {
   return {
-    product: state.products.product
+    product: state.products.product,
+    cart: state.cart
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     fetchProduct: (id) => dispatch(fetchProduct(id)),
+    addProduct: (productId, qty, price) =>
+      dispatch(addOrUpdateProduct(productId, qty, price))
   }
 }
 
